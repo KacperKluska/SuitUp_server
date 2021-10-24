218b04b44c3e597d46ceb2b19b7a1b8b3f59b907
KacperKluska <kacperkluska130@gmail.com>
2021-10-24 15:41:59 +0200

    added cookieParser to app

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
+app.use(cookieParser());
 
 const { connect } = require("./database/connect");
 
